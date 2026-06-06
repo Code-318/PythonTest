80b92d60a2a20638ef48ebc6120848fba330cdfa
MCF <samuel.l.winterton@gmail.com>
2026-06-06 20:23:13 +0100

    Create Script2.js

---
+++ Script2.js
@@ -0,0 +1,18 @@
+const PRESETS = {
+    bible: {
+      theme: 'BIBLE BOOKS',
+      items: ['Genesis','Exodus','Leviticus','Numbers','Deuteronomy','Joshua','Judges','Ruth','1 Samuel','2 Samuel','1 Kings','2 Kings','1 Chronicles','2 Chronicles','Esther','Job','Psalms','Proverbs','Ecclesiastes','Isaiah','Jeremiah','Lamentations','Ezekiel','Daniel','Jonah','Matthew','Mark','Luke','John','Acts','Romans','1 Corinthians','2 Corinthians','Galatians','Ephesians','Philippians','Colossians','1 Thessalonians','2 Thessalonians','1 Timothy','2 Timothy','Titus','Philemon','Hebrews','James','1 Peter','2 Peter','1 John','2 John','3 John','Jude','Revelation']
+    },
+    jobs: {
+      theme: 'OCCUPATIONS',
+      items: ['Doctor','Pilot','Chef','Spy','Teacher','Police Officer','Actor','Engineer','Journalist','Lawyer','Farmer','Astronaut','Firefighter','Sailor','Musician']
+    },
+    politics: {
+      theme: 'POLITICIANS',
+      items: ['Victor Orban','Donald Trump','Boris Johnson','Keir Starmer','Margaret Thatcher','Putin','Xi Jinping','Kim Jong Un','Kamala Harris','Barack Obama','John F Kennedy','Nixon','John Major','Nigel Farage','Rupert Lowe','Ronald Reagan','Rishi Sunak','Benjamin Netanyahu','Tony Blair','Rory Stewart','Winston Churchill','Kemi Badenoch','Ed Miliband']
+    },
+    movies: {
+      theme: 'MOVIES',
+      items: ['Titanic','The Matrix','Inception','Interstellar','The Godfather','Parasite','Avatar','Pulp Fiction','The Dark Knight','Jurassic Park']
+    }
+  };
